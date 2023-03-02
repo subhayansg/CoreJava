a7af34f102ec2080100108445a9e87dc71fa3457
Subhayan Ghosh <subhayan.learn@gmail.com>
2023-03-02 20:26:11 +0530

    Dynamic method dispatch

---
+++ src/Override.java
@@ -0,0 +1,25 @@
+class Super {
+    public void display() {
+        System.out.println("Super Display");
+    }
+}
+
+class Sub extends Super {
+    public void display() {    // overridden method
+        System.out.println("Sub Display");
+    }
+
+    public void newDisplay() {
+        System.out.println("New display");
+    }
+}
+
+public class Override {
+    public static void main(String[] args) {
+        Super sup = new Sub();  // Super reference, sub class object
+//	  Sub s = new Super();    // Sub reference, super class object NOT POSSIBLE
+        sup.display();          // method available in Super class, allowed
+//        sup.newDisplay();       // method not available in Super class, NOT ALLOWED
+
+    }
+}
